5244f935d3ee85972cf207e7a2b3ef98b01c5d9a
elensalustiano <elen.salustiano@gmail.com>
2021-02-02 18:16:33 -0300

    feat: add create header button

---
+++ components/header-button/index.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { StyleSheet, TouchableOpacity, Text } from 'react-native'
+
+import { Colors, FontSize } from '../../constants/Styles'
+
+type AddButtonProps = {
+  onPress: () => void
+}
+
+export default function AddButton({ onPress }: AddButtonProps) {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>+</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+  },
+  text: {
+    color: Colors.primaryColor,
+    fontSize: FontSize.extraLarge
+  }
+})
